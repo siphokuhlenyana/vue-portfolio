3bc108fae1fb5f6e328336afc505b581cf816afd
Siphokuhle <siphokuhlenyana@gmail.com>
2024-07-01 12:06:32 +0200

    :construction:

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+ <home-comp/>
 </template>
+<script>
+import HomeComp from './components/HomeComp.vue'
 
+export default{
+  components:{
+    HomeComp
+
+  }
+}
+
+
+
+
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -15,16 +24,5 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
